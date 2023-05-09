06f2014d554c1ffc0dff32549991b30800e0c210
Cristian Steven Arévalo Mesa <Cristian.ArevaloM.ext@claro.com.co>
2023-05-09 13:43:13 -0500

    ejercicio4

---
+++ Ejercicio4.js
@@ -0,0 +1,8 @@
+//tipos de datos
+let nombre="Nombre";
+let apellido="Apellido";
+let edad=24;
+let cancion;
+let entendioTema=true;
+
+console.log(`Hola! Mi nombre es ${nombre} ${apellido} tengo edad años, mi canción favorita es ${cancion} y la y la respuesta es ${entendioTema}`)
