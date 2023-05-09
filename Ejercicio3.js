c3f372d5146456359f822b0bd92dac162b29b9c4
Cristian Steven Arévalo Mesa <Cristian.ArevaloM.ext@claro.com.co>
2023-05-09 13:28:25 -0500

    Ejercicio 3

---
+++ Ejercicio3.js
@@ -0,0 +1,4 @@
+const SEGUNDOS_MINUTO=60;
+const MINUTOS_HORA=60;
+const HORAS_DIA=24;
+console.log(`Un minuto tiene ${SEGUNDOS_MINUTO} segundos, una hora tiene ${MINUTOS_HORA} minutos y un día tiene ${HORAS_DIA} horas`)
